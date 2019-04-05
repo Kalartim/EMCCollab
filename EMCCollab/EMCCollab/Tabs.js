80acc388764149de64e16ecd84583157533ccf37
Marc Bonilla <marc.bonilla@mymail.champlain.edu>
2019-04-05 14:13:47 -0400

    Adding functional tabs.

--- EMCCollab/EMCCollab/Tabs.js
+++ EMCCollab/EMCCollab/Tabs.js
@@ -1,21 +1,21 @@
 ﻿// Tab Function
-function tabScript(evt, title) {
+function tabScript(event, title) {
 
     var i, tabcontent, tablinks;
 
     // Gets all elements in tabcon class and hides them
-    tabcontent = document.getElementsByClassName("tabcont");
-    for (i = 0; i < tablinks.length; i++) {
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
     }
 
     //Gets all elements in tablinks class and removes them from the active class
     tablinks = document.getElementsByClassName("tablinks");
     for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace("active", "");
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
 
     //Show the  current tab, and add the active class to the button that opens that tab.
     document.getElementById(title).style.display = "block";
-    evt.currentTarget.className += "active";
+    event.currentTarget.className += " active";
 }
